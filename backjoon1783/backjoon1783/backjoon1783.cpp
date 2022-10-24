5bf70fdb7040f6031bed956757d8d6d557b2fd3e
awardfact <awardfact@hanmail.net>
2022-10-24 22:52:04 +0900

    1024

---
+++ backjoon1783/backjoon1783/backjoon1783.cpp
@@ -0,0 +1,81 @@
+﻿// backjoon1783.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+/*
+* 
+* 
+
+위아래가 충분할 때 가능한 수는 1 + (N -2)
+세로는 3개 이상이면 모두 가능 3개 이하면 최대 3개 
+
+5개 미만인 경우에는 같은 경우의 수만 이용이 가능 
+
+세로가 1이거나 가로 1이면 1
+
+세로가 2인 경우 
+가로가 7 이상이면 4 5이상이면 3 3이상이면 2 나머지 1
+
+세로가 3 이상인 경우
+가로가 1이면 1
+가로가 2면 2
+가로가 3이면 3 
+가로가 4면 4
+가로가 7 이상이면  N-2 
+*/
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int count = 0;
+    if (n >= 3) {
+
+        if (m >= 7)
+        {
+            count = m - 2;
+        }
+        else if (m >= 4) {
+            count = 4;
+        }
+        else if (m >= 3) {
+            count = 3;
+        }
+        else if (m >= 2) {
+            count = 2;
+        }
+        else {
+            count = 1;
+        }
+        
+    }
+    else if(n == 2) {
+        
+        if (m >= 7) {
+
+            count = 4;
+        }else if (m >= 5) {
+            count = 3;
+        }
+        else if(m >= 3) {
+            count = 2;
+        }
+        else {
+            count = 1;
+        }
+
+
+    }
+    else {
+        count = 1;
+    }
+        
+
+    cout << count << "\n";
+
+    return 0;
+}
+
+
---
+++ backjoon7795/backjoon7795/backjoon7795.cpp
@@ -0,0 +1,100 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+int A[20000] = { 0, };
+int B[20000] = { 0, };
+
+/*
+
+T를 입력받은
+T개수만큼 반복문을 돌림
+N,M을 입력받음
+N개 반복, M개 반복문을 돌려서 입력받음
+숫자를 A,B배열에 넣음
+B 내림차순으로 정렬하고
+비교값보다 작은 위치 + 1 을 리턴하는 이진탐색함수 생성
+A반복 돌리고 함수 호출해서 M - 리턴값 을 카운팅한다
+*/
+
+bool cmp(int a, int b) {
+    return a > b;
+}
+
+int ijin(int left, int right, int cmp, int c[]) {
+    int mid = (left + right) / 2;
+
+    if (c[mid] < cmp) {
+        if (mid == 0) {
+            return 0;
+        }
+        else {
+
+            return ijin(left, mid -1, cmp, c);
+
+        }
+    }
+    else {
+
+        if (mid == right) {
+            return mid + 1;
+        }
+        else {
+            return ijin(mid + 1, right, cmp, c);
+        }
+    }
+
+}
+
+int main()
+{
+    int T, N, M;
+
+   // list<int> A;
+    //list<int> B;
+
+    int tmp;
+    cin >> T;
+
+
+    int mid, left, right;
+    int count = 0;
+    for (int i = 0; i != T; i++) {
+        cin >> N >> M;
+        count = 0;
+        for (int j = 0; j < N; j++) {
+
+            cin >> tmp;
+            //A.push_back(tmp);
+            A[j] = tmp;
+        }
+        for (int j = 0; j < M; j++) {
+            cin >> tmp;
+            //B.push_back(tmp);
+            B[j] = tmp;
+
+        }
+
+        sort(B, B + M , cmp);
+        mid = M / 2;
+        left = 0;
+        right = M -1;
+
+        for (int j = 0; j != N; j++) {
+            tmp = ijin(left, right, A[j], B);
+
+            count += M - tmp;
+
+
+        }
+
+        cout << count << "\n";
+
+
+    }
+    return 0;
+}
